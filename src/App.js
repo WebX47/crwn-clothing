bf9b42a4b4af1d794a6e41be974f16489761bcb6
Akash <akashch47@gmail.com>
2022-12-03 19:52:02 +0530

    section 6 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/Navigation/Navigation";
+import Authentication from "./routes/Authentication/Authentication";
 import Home from "./routes/Home/Home";
 
 const Shop = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
