01605d7fe616457e58168a2d3067caaadbc6f61e
Akash <akashch47@gmail.com>
2022-12-06 01:33:01 +0530

    section 9 done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/Navigation/Navigation";
 import Authentication from "./routes/Authentication/Authentication";
 import Home from "./routes/Home/Home";
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Shop from "./routes/Shop/Shop";
+import Checkout from "./routes/Checkout/Checkout";
 
 const App = () => {
   return (
@@ -14,6 +12,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/Shop/Shop.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../context/products.context";
+import ProductCard from "../../components/ProductCard/ProductCard";
+
+import "./Shop.scss";
+
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
+
+  return (
+    <div className="products-container">
+      {products.map((product) => {
+        return <ProductCard key={product.id} product={product} />;
+      })}
+    </div>
+  );
+};
+
+export default Shop;
