45c064dbf92ac770c0afd6839d3983a827f2e8af
Akash <akashch47@gmail.com>
2022-12-07 07:07:04 +0530

    section 10 done

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,18 +1,16 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../context/products.context";
-import ProductCard from "../../components/ProductCard/ProductCard";
+import { Routes, Route } from "react-router-dom";
+
+import CategoriesPreview from "../CategoriesPreview/CategoriesPreview";
+import Category from "../Category/Category";
 
 import "./Shop.scss";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
-
   return (
-    <div className="products-container">
-      {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />;
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
